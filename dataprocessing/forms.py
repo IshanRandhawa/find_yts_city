968357421ee17dc8d4f438f9750ccc33fda3916b
Ishan <ishan18madras@gmail.com>
2020-06-20 03:00:52 +0530

    Added basic bootstrap

--- dataprocessing/forms.py
+++ dataprocessing/forms.py
@@ -2,9 +2,19 @@
 from django import forms 
 
 from .models import userinput
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm
 
 class userinput(forms.ModelForm): 
     class Meta: 
         model = userinput 
-        fields = "__all__"
+        fields = ('query','city','number_queries')
+    
+        widgets = {
+            'query': forms.TextInput(attrs={'class': 'form-control' , 'placeholder':'Your interests'}),
+            'city': forms.Select(attrs={'class': 'form-control'}),
+            'number_queries':forms.NumberInput(attrs={'class': 'form-control', 'type':'range','min':"1", 'max':"50" ,'onchange':"updateTextInput(this.value)"}),
+        }
+
+        # help_texts = {
+        #     'query': ('Type what type of youtubers you are looking for'),
+        # }
