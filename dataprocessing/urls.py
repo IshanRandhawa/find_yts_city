3865937da97887c2039818826a5ccfe5514b6b19
Ishan <ishan18madras@gmail.com>
2020-06-28 23:51:22 +0530

    fixes template

--- dataprocessing/urls.py
+++ dataprocessing/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'dataprocessing'
 urlpatterns = [ 
     path('', views.home_view, name = 'home'),
+    path('home', views.home_view, name = 'home'),
     path('database' , views.table, name = 'database'),
     path('subtable/query/city', views.subtable, name ='subtable')
     # path('subtable' , views.subtable, name = 'subtable'),
